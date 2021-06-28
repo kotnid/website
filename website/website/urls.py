de6244af16413412d7a66226b1836732277fcd55
kotnid <tkt0506tkt@gmail.com>
2021-06-28 10:22:15 +0800

    add 2 pages

--- website/website/urls.py
+++ website/website/urls.py
@@ -20,5 +20,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('' ,views.index , name="index")
+    path('' ,views.index , name="index"),
+    path('about' ,views.about , name="about"),
+    path('contact' ,views.contact , name="contact")
 ]
--- website/website/views.py
+++ website/website/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import render
 def index(request):
     return render(request , 'index.html')
 
+def about(request):
+    return render(request , 'about.html')
 
-
-
+def contact(request):
+    return render(request , 'contact.html')
 
 

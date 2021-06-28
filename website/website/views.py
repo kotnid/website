ec080dce9cf950450fad4d7d7ca450e9c879a005
kotnid <tkt0506tkt@gmail.com>
2021-06-28 10:17:24 +0800

    yep

--- website/website/views.py
+++ website/website/views.py
@@ -1,5 +1,10 @@
-from django.http import HttpResponse
+from django.shortcuts import render 
 
 def index(request):
-    return HttpResponse("u sus")
+    return render(request , 'index.html')
+
+
+
+
+
 

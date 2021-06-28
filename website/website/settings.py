98a1bf8df68129eaafe885f3ce70ca1fc56dd2c2
kotnid <tkt0506tkt@gmail.com>
2021-06-28 14:53:30 +0800

    create module

--- website/website/settings.py
+++ website/website/settings.py
@@ -21,7 +21,7 @@ PROJECT_DIR = os.path.join(BASE_DIR, "website")
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "lol"
+SECRET_KEY = "django-insecure-)olt)ccsvzd!d1e-m@t^a!o@fc*d@36@9-m_z(iky$36%n*-jz"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "website.apps.accounts"
+
 ]
 
 MIDDLEWARE = [

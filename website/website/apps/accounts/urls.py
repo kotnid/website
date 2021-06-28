5155430f4a2bfcc734304129f0fa5e8e5d149ed9
kotnid <tkt0506tkt@gmail.com>
2021-06-28 13:59:10 +0800

    structure change

---
+++ website/website/apps/accounts/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path , include
+from django.contrib.auth import views as auth_views
+
+from . import views
+
+app_name = "accounts"
+urlpatterns = [
+    path("profile", views.ProfileView.as_view(), name="profile"),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(template_name="accounts/login.html"),
+        name="login",
+    ),
+    path("logout", auth_views.LogoutView.as_view(), name="logout"),
+]
---
+++ website/website/apps/accounts/views.py
@@ -0,0 +1,6 @@
+from django.views.generic.base import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+class ProfileView(LoginRequiredMixin,TemplateView):
+    template_name = 'accounts/profile.html'
